b415d7e92fdb6759a8e1ec09c0d3dff4a9fd42c6
Svan Yao (bayao) <bayao@cisco.com>
2017-06-05 16:54:58 +0800

    clear no used files

--- manage.py
+++ manage.py
@@ -8,10 +8,6 @@ app = create_app('default')
 manager = Manager(app)
 
 
-def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
-manager.add_command("shell", Shell(make_context=make_shell_context))
-
 from app.models import db
 db.init_app(app)
 
