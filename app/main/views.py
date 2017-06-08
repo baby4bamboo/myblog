70d41c40d2c7923d760588236d345304ec91aede
Svan Yao (bayao) <bayao@cisco.com>
2017-06-08 09:08:53 +0800

    modify user to initdb in view.py

--- app/main/views.py
+++ app/main/views.py
@@ -20,14 +20,14 @@ def index():
     return render_template('index.html', todos=todos)
 
 
-@main.route("/user")
-def user():
+@main.route("/initdb")
+def initdb():
     # As a list to test debug toolbar
     User.objects().delete()  # Removes
     User(email="bayao@cisco.com", password="123", password_hash=User.generate_password_hash("123"), username="Svan Yao",group="subscriber").save()  # Insert
     User(email="yunjie@gmail.com", password="456",  password_hash=User.generate_password_hash("456"),username="Yun Jie",group="subscriber").save()  # Insert
     users = User.objects.all()
-    return render_template('user.html', users=users)
+    return render_template('initdb.html', users=users)
 
 
 @main.route("/pagination")
